c10f9c7e561afca6d374c5c282c80d2264274232
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2022-02-12 14:44:59 +0000

    added api requst

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Intro from "./components/Intro";
 import QuestionList from "./components/QuestionList";
 
 function App() {
   const [newGame, setNewGame] = useState(false);
-  const [quizQuestions, setQuizQuestions] = useState();
+  const [quizQuestions, setQuizQuestions] = useState([]);
 
   function startGame() {
     setNewGame(!newGame);
-    setQuizQuestions(
-      tempData.map((item) => {
-        const correctAnswer = item.answer;
-        const choices = [...item.wrongAnswers, correctAnswer];
-
-        return {
-          id: item.id,
-          question: item.question,
-          answer: correctAnswer,
-          choices: choices.sort(() => Math.random() - 0.5),
-        };
-      })
-    );
   }
+
+  function resetGame() {
+    startGame();
+    fetchQuizData();
+  }
+
+  function fetchQuizData() {
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+      .then((res) => res.json())
+      .then((result) => {
+        setQuizQuestions(
+          result.results.map((item) => {
+            const correctAnswer = item.correct_answer;
+            const choices = [...item.incorrect_answers, correctAnswer];
+            return {
+              id: item.question,
+              question: item.question,
+              correctAnswer: correctAnswer,
+              choices: choices.sort(() => Math.random() - 0.5),
+            };
+          })
+        );
+      });
+  }
+
+  useEffect(() => {
+    fetchQuizData();
+  }, []);
 
   return (
     <main className="container">
       {newGame ? (
-        <QuestionList quizQuestions={quizQuestions} startGame={startGame} />
+        <QuestionList quizQuestions={quizQuestions} resetGame={resetGame} />
       ) : (
         <Intro startGame={startGame} />
       )}
@@ -35,33 +50,33 @@ function App() {
   );
 }
 
-const tempData = [
-  {
-    id: 1,
-    question: "What is 2+2?",
-    answer: "4",
-    wrongAnswers: ["2", "6", "3"],
-  },
+// const tempData = [
+//   {
+//     id: 1,
+//     question: "What is 2+2?",
+//     answer: "4",
+//     wrongAnswers: ["2", "6", "3"],
+//   },
 
-  {
-    id: 2,
-    question: "What is hello in french?",
-    answer: "Bonjour",
-    wrongAnswers: ["hola", "nein", "eh?"],
-  },
-  {
-    id: 3,
-    question: "question 3",
-    answer: "bingo",
-    wrongAnswers: ["lol", "never", "dunno pal"],
-  },
+//   {
+//     id: 2,
+//     question: "What is hello in french?",
+//     answer: "Bonjour",
+//     wrongAnswers: ["hola", "nein", "eh?"],
+//   },
+//   {
+//     id: 3,
+//     question: "question 3",
+//     answer: "bingo",
+//     wrongAnswers: ["lol", "never", "dunno pal"],
+//   },
 
-  {
-    id: 4,
-    question: "question 4?",
-    answer: "answer456",
-    wrongAnswers: ["anser123", "answer898?", "answer976"],
-  },
-];
+//   {
+//     id: 4,
+//     question: "question 4?",
+//     answer: "answer456",
+//     wrongAnswers: ["anser123", "answer898?", "answer976"],
+//   },
+// ];
 
 export default App;
