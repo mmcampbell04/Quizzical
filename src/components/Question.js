859021878fcfbddac9e24e46fc2019b0e5fffd84
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2022-02-12 14:45:20 +0000

    added logic to change label for right/wrong answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,29 +5,55 @@ export default function Question({
   handleChange,
   userData,
   isDisabled,
+  isGameOver,
 }) {
   let answers = question.choices.map((choice, index) => {
     return (
       <div className="answer" key={index}>
-        <input
-          id={`option-${choice}`}
-          type="radio"
-          className="quiz-answers"
-          name={question.question}
-          value={choice}
-          checked={userData[question.question] === choice}
-          onChange={handleChange}
-          disabled={isDisabled}
-        />
-        <label htmlFor={`option-${choice}`}>{choice}</label>
+        <label
+          className={`${
+            // is the game over?
+            // if yes and the input is the correct ansewr set it to green
+            // if yes & the user answer is not the corerct answer set it to red
+            isGameOver
+              ? choice === question.correctAnswer
+                ? "label-right"
+                : userData[question.id] === choice
+                ? "label-wrong"
+                : ""
+              : userData[question.id] === choice
+              ? "selected"
+              : ""
+          }`}
+        >
+          {choice}
+          <input
+            id={`option-${choice}`}
+            type="radio"
+            className="quiz-answers"
+            name={question.id}
+            value={choice}
+            answer={question.answer}
+            checked={userData[question.question] === choice}
+            onChange={handleChange}
+            disabled={isDisabled}
+          />
+        </label>
       </div>
     );
   });
 
+  function cleanupQuestion() {
+    return { __html: question.question };
+  }
+
   return (
     <div className="question-card">
-      <h2>{question.question}</h2>
+      <h2 dangerouslySetInnerHTML={cleanupQuestion()}></h2>
       <div className="answers-container">{answers}</div>
     </div>
   );
 }
+
+// if the game is over - the correct answer is green
+// if the game is over & the user answer is not the correct answer, it is red
